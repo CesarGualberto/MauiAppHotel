a7bc8e91a3f57dc9bfe8cf68491392211cd76d90
César Gualberto <cjgualberto824@gmail.com>
2024-11-06 20:26:51 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+       public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Súite Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+            
+            new Quarto()
+            {
+                Descricao = "Súite Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+            },
+            
+            new Quarto()
+            {
+                Descricao = "Súite Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+            },
+
+            new Quarto()
+            {
+                Descricao = "Súite Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var Window = base.CreateWindow(activationState);
+
+            Window.Width = 400;
+            Window.Height = 600;
+
+            return Window;
+        }
+    }
+}
